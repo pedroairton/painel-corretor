ca2068c6651ba5357ba74efcac3b153f0e1e7263
pedroairtonz <pedroairton@zaite.com.br>
2026-08-31 10:39:12 -0300

    Api url production

--- src/services/api.client.ts
+++ src/services/api.client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: window.location.hostname === 
+  "localhost" ? "http://localhost:8000/api/v1" : "https://painel-corretor.srv1109011.hstgr.cloud/api/v1",
   timeout: 5000,
   headers: {
     "Content-type": "application/json",
